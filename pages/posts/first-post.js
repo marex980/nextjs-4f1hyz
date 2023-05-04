2b10d6d91803e326eeb735c0f193f8ab0af472e2
marex980 <69250892+marex980@users.noreply.github.com>
2023-05-04 10:39:37 +0200

    1 file modified

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,15 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 function prviPost() {
   return (
     <>
+      <Head>
+        <title>Moj prvi post</title>
+      </Head>
       <h1>Prvi post</h1>
       <h2>
-        <Link href="/">Home</Link>
+        <Link href="/">← Home</Link>
       </h2>
     </>
   );
